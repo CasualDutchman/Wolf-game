1d683b375ef0cb51e613e56d0597e55f155061b0
Pieter <casual.dutchman@live.nl>
2018-04-24 12:45:13 +0200

    ui mockup implementation

---
+++ Assets/Scripts/SeasonChanging.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonChanging : MonoBehaviour {
+
+    public Material material;
+    public float changeSeasonTime = 5;
+
+    float timer;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        float one = 1 / changeSeasonTime;
+        timer += Time.deltaTime * one;
+
+        if (timer >= 4) {
+            timer = 0;
+        }
+
+        material.SetFloat("_Blend", timer);
+	}
+}
